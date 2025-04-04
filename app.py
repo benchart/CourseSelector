c4017d0c4f8e4073cffa28295ba0992098f4050b
hjac201 <huntjac@iu.edu>
2025-04-04 19:39:56 -0400

    First AI import

--- app.py
+++ app.py
@@ -1,1 +1,3 @@
-print('hello')
+import ollama
+for part in ollama.chat(model='mistral', messages=[{'role': 'user', 'content': 'Write me a Haiku about your favorite programming language'}], stream=True):
+        print(part['message']['content'], end='', flush=True)
