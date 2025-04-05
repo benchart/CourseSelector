89aba77f2e0aadab376e477a91a7c1e1c6810322
Parley <pih2549@gmail.com>
2025-04-05 15:11:44 -0400

    chatbot connect WORKING

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .chatbotModel import ChatbotModel
 
-# Create your views here.
+def chatbot_view(request):
+    response = None
+    if request.method == "POST":
+        user_input = request.POST.get("message")
+        if user_input:
+            model = ChatbotModel()
+            response = model.does_not_match(user_input)  # or model.callChatbot(user_input) if fully functional
+
+    return render(request, "chatbot.html", {"response": response})
+
