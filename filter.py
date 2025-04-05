30fa9223cc56e19dd68cc487ebc4ec50bc3e0f57
hjac201 <huntjac@iu.edu>
2025-04-05 10:30:18 -0400

    Test input to userData

--- filter.py
+++ filter.py
@@ -1,6 +1,12 @@
 from chatbotModel import ChatbotModel
+from userManagement import UserManagement
+
+
 model = ChatbotModel()
 model.callChatbot("whats a funny joke mike")
 model.callChatbot("tell me another funny joke")
 model.callChatbot("hello george")
 model.callChatbot("please list out all of the class descriptions")
+
+userSystem = UserManagement()
+userSystem.createNewUser("Parley Hartwell", "phartwell", 19, "Freshman", 18)
