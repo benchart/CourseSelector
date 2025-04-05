218cb780bfc3afdde2b39797e1c08d6283acf742
benchart <benchart@iu.edu>
2025-04-05 11:09:05 -0500

    dataset

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/student', views.signup_student, name='signup_student'),
     path('signup/admin', views.signup_admin, name='signup_admin'),
     path('select-interests', views.select_interests, name='select_interests'),
+    path("select-interests/", views.select_interests, name="select_interests"),
 ]
--- smart_course_selector/settings.py
+++ smart_course_selector/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
+    'students',
+    'admins',
+    'courses',
+    'chatbot',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
